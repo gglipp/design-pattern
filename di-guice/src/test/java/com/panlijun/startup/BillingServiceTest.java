90ca59b9eb5532af83af856c2e624537ef9342f6
panlijun <panlijun@vip.126.com>
2019-07-31 23:02:20 +0800

    di practise: guice startup : factory refactor test

--- di-guice/src/test/java/com/panlijun/startup/BillingServiceTest.java
+++ di-guice/src/test/java/com/panlijun/startup/BillingServiceTest.java
@@ -1,22 +1,57 @@
 package com.panlijun.startup;
 
 import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BillingServiceTest {
 
+    private final PizzaOrder order = new PizzaOrder(100);
+    private final CreditCard creditCard = new CreditCard();
+
+    private final TransactionLog transactionLog = new DatabaseTransactionLog();
+    private final CreditCardProcessor processor = new PaypayCreditCardProcessor();
+
+    @Before
+    public void setUp() {
+        TransactionLogFactory.setInstance(transactionLog);
+        CreditCardProcessorFactory.setInstance(processor);
+
+    }
+
+
+//    @Test
+//    public void should_get_successful_receipt_if_charge_successfully(){
+//        //given
+//        BillingService billingService = new RealBillingService();
+//        PizzaOrder order = new PizzaOrder(100);
+//        CreditCard creditCard = new CreditCard();
+//
+//        //when
+//        Receipt receipt = billingService.chargeOrder(order, creditCard);
+//
+//        //then
+//        Assertions.assertThat(receipt.getMessage()).isEqualTo(Message.CHARGE_SUCCESSFULLY);
+//
+//    }
+
     @Test
-    public void should_get_successful_receipt_if_charge_successfully(){
-        //given
+    public void should_get_successful_receipt_if_charge_successfully_with_factory(){
+       //given
         BillingService billingService = new RealBillingService();
-        PizzaOrder order = new PizzaOrder(100);
-        CreditCard creditCard = new CreditCard();
 
         //when
         Receipt receipt = billingService.chargeOrder(order, creditCard);
 
-        //then
+       //then
         Assertions.assertThat(receipt.getMessage()).isEqualTo(Message.CHARGE_SUCCESSFULLY);
+    }
+
+    @After
+    public void tearDown() {
+        TransactionLogFactory.setInstance(null);
+        CreditCardProcessorFactory.setInstance(null);
 
     }
 }
